35c98298a50de3149d4697617111cc4366515205
RainMark <rain.by.zhou@gmail.com>
2021-02-08 20:34:54 +0800

    add lld/gold test
    Signed-off-by: RainMark <rain.by.zhou@gmail.com>

--- split-stack/build.sh
+++ split-stack/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cc=clang++
-FLAGS="-O0 -std=c++14 -fno-inline"
+# FLAGS="-O0 -fuse-ld=lld -std=c++14 -fno-inline"
+# FLAGS="-O0 -fuse-ld=gold -Wl,--split-stack-adjust-size=314572800 -std=c++14 -fno-inline"
+FLAGS="-O0 -std=c++14 -fno-inline" # use ld
 
 # $cc -shared -fsplit-stack $FLAGS foo.cpp -o libfoo.so
 $cc -shared $FLAGS foo.cpp -o libfoo.so
--- split-stack/foo.cpp
+++ split-stack/foo.cpp
@@ -1,6 +1,6 @@
 #include "foo.h"
 
 int foo(int n) {
-    char b[1024 * 512] = {0};
+    char b[1024 * 1024 * 200] = {0};
     return n + 1;
 }
